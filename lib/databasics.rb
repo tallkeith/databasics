6db950680a6bfb6c1a3fad567bd16ee2a02024ae
tallkeith <tallestkeith@gmail.com>
2016-03-22 15:11:36 -0400

    added item and order models

--- lib/databasics.rb
+++ lib/databasics.rb
@@ -5,11 +5,27 @@ require "pry"
 require "databasics/version"
 require "databasics/user"
 require "databasics/address"
+require "databsics/item"
+require "databasics/order"
 
 db_config = YAML.load(File.open("config/database.yml"))
 ActiveRecord::Base.establish_connection(db_config)
 
 module Databasics
+	class App
+
+		def user_info
+			puts "What is your first name?"
+			first_name=gets.chomp
+			puts "What is your last name?"
+			last_name=gets.chomp
+			puts "What is your email?"
+			email=gets.chomp
+
+			
+		end
+		# define 4 methods from hw notes
+	end
   # Your code goes here...
 
   ## 3 ActiveRecord querying methods

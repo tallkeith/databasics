fa70bf35a794f8e3effdc2ded8a127dcc62cbffd
Brit Butler <redline6561@gmail.com>
2016-03-22 11:15:27 -0400

    Add config gunk to wire ActiveRecord to the DB.

--- lib/databasics.rb
+++ lib/databasics.rb
@@ -1,4 +1,14 @@
+require "active_record"
+require "yaml"
+require "pry"
+
 require "databasics/version"
+require "databasics/user"
+
+db_config = YAML.load(File.open("config/database.yml"))
+ActiveRecord::Base.establish_connection(db_config)
+
+binding.pry
 
 module Databasics
   # Your code goes here...
